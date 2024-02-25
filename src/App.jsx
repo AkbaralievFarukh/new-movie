0c1ebe842afbf4501760b62e74d3ece1c0a4b6a6
Farukh <akbfarukh@gmail.com>
2024-02-25 17:29:46 +0600

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React, {useEffect} from 'react';
-import {Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/home/home.jsx";
 import Movie from "./pages/movie/movie.jsx";
 const App = () => {
 
     return (
-        <Routes>
-            <Route path={'/'} element={<Home/>}/>
-            <Route path={'/movie/:id'} element={<Movie/>}/>
-        </Routes>
+        <BrowserRouter>
+            <Routes>
+                <Route path={'/'} element={<Home/>}/>
+                <Route path={'/movie/:id'} element={<Movie/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
