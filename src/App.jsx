b011a83a6e29feb671847be9f9b91036ba8b784a
Farukh <akbfarukh@gmail.com>
2024-02-25 17:35:46 +0600

    fix browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React, {useEffect} from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/home/home.jsx";
 import Movie from "./pages/movie/movie.jsx";
 const App = () => {
 
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path={'/'} element={<Home/>}/>
                 <Route path={'/movie/:id'} element={<Movie/>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 };
 
